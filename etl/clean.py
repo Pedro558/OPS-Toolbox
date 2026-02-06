b3d4d888ddad3b246f870ddd892c525157205218
pedro558 <pedroafonso109@gmail.com>
2026-02-06 01:33:27 -0300

    refactor: simplify remove_empty_values function

--- etl/clean.py
+++ etl/clean.py
@@ -20,19 +20,11 @@ def remove_status_column(df: pd.DataFrame) -> pd.DataFrame:
 
 def remove_empty_values(df: pd.DataFrame) -> pd.DataFrame:
   """Remove all rows with empty values in all columns"""
-  df = df.dropna(how="all")
-  
-  """Replace 'Missing Values' with the ' - ' symbol """
-  #df = df.fillna(" - ")
+  df = df.dropna()
   return df
 
 def convert_inventory_types(df: pd.DataFrame) -> pd.DataFrame:
   """Change inventory column types"""
   df["quantity"] = pd.to_numeric(df["quantity"], errors="coerce")
   df["min_quantity"] = pd.to_numeric(df["min_quantity"], errors="coerce")
-  
-  #df = df.astype({
-  #  "quantity": 'int64',
-  #  "min_quantity": 'int64'})
-  
   return df
