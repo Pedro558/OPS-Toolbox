6f1c7746367043c6da0ccdfdb4a38f724eccd401
pedro558 <pedroafonso109@gmail.com>
2026-02-07 11:45:46 -0300

    feat: add one column and format numbers

--- etl/clean.py
+++ etl/clean.py
@@ -27,4 +27,22 @@ def convert_inventory_types(df: pd.DataFrame) -> pd.DataFrame:
   """Change inventory column types"""
   df["quantity"] = pd.to_numeric(df["quantity"], errors="coerce")
   df["min_quantity"] = pd.to_numeric(df["min_quantity"], errors="coerce")
+
+  for col in ["quantity", "min_quantity"]:
+    if col in df.columns:
+    # Se todas as partes decimais forem .0, converte para inteiro nullable
+      if (df[col].dropna() % 1 == 0).all():
+        df[col] = df[col].astype("Int64")
+
+  return df
+
+def create_material_column(df: pd.DataFrame) -> pd.DataFrame:
+  """Create a new 'material' column based on the 'cable_length' values"""
+  df = df.copy()
+  
+  is_header = df["quantity"].isna() & df["min_quantity"].isna() & df["cable_length"].notna()
+
+  df["material"] = df["cable_length"].where(is_header)
+  df["material"] = df["material"].ffill()
+
   return df
