82da898d85822ea4149bb8de0e3bb81884735450
pedro558 <pedroafonso109@gmail.com>
2026-04-07 14:04:32 -0300

    refactor: rename and improve find_last_cable_position function for clarity

--- etl/table_formatter.py
+++ etl/table_formatter.py
@@ -1,22 +1,27 @@
 import pandas as pd
+import numpy as np
 
-def find_last_cable_index(df: pd.DataFrame) -> pd.DataFrame:
-  # Identify the last line from 'cable' material
+def find_last_cable_position(df: pd.DataFrame) -> int:
+  """Identify the last line from 'cable' material"""
   mask_last_cable = df["material"].astype("string").str.contains("cord", case=False, na=False)
-  last_index = df[mask_last_cable].index[-1]
+
+  positions = np.flatnonzero(mask_last_cable)
+  if len(positions) > 0:
+    last_position = positions[-1]
+  else: 
+    last_position = None
+    raise ValueError("No 'cable' materials found in the DataFrame.")
   
-  return last_index
+  return last_position
 
 def format_inventory_cables(df: pd.DataFrame) -> pd.DataFrame:
-  last_index = find_last_cable_index(df)
+  """Format inventory DataFrame to only include 'cable' materials with standardized columns and types"""
+  last_position = find_last_cable_position(df)
 
-  # Find header rows
   is_header = df["quantity"].isna() & df["min_quantity"].isna() & df["cable_length"].notna()
 
-  # Create new DF only with 'cables'
-  inventory_cables = df.iloc[0:last_index]
+  inventory_cables = df.iloc[:last_position + 1].copy()
 
-  # Remove 'metros' from 'cable_length' column and convert to float
   cable_length_raw = inventory_cables["cable_length"].where(~is_header)
   cable_length_num = (
     cable_length_raw.astype("string")
@@ -33,14 +38,10 @@ def format_inventory_cables(df: pd.DataFrame) -> pd.DataFrame:
   return inventory_cables
 
 def format_inventory_materials(df: pd.DataFrame) -> pd.DataFrame:
-  last_index = find_last_cable_index(df)
-    
-  # Identify the last line from 'cable' material
-  mask_last_cable = df["material"].astype("string").str.contains("cord", case=False, na=False)
-  last_index = df[mask_last_cable].index[-1]
+  """Format inventory DataFrame to only include 'non-cable' materials with standardized columns and types"""
+  last_position = find_last_cable_position(df)
 
-  # New DF with other materials
-  inventory_materials = df.iloc[last_index+1:]
+  inventory_materials = df.iloc[last_position + 1:].copy()
   inventory_materials = inventory_materials.rename(columns={
   "material": "material_group",
   "cable_length": "material"
